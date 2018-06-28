8b2ee68d765802bb4f8c672c1fe29d89c5ecf09b
Petemab <peteben18@hotmail.com>
2018-06-28 16:54:29 +0100

    carousel more or less working. Nav bar fixed. Adding info to portfolio sections

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
+$(document).ready(function(){
 
+  $('.navbar-burger').click(function() {
+    $('#navbarMenuHeroA, .navbar-burger').toggleClass('is-active');
+  });
 
-  // var carousels = bulmaCarousel.attach();
-
+  $('.workCarousel').slick({
+    dots: true,
+    infinite: true,
+    variableWidth: true,
+    dotsClass: 'slick-dots',
+    centerMode: true,
+    centerPadding: '10px',
+    slidesToShow: 1,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
 });
