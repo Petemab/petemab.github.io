a956ebaa99845ddab2b9db472442ba2e1be466cf
Petemab <peteben18@hotmail.com>
2018-06-29 10:56:07 +0100

    carousel working. Fixed a couple of spelling mistakes too.

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     variableWidth: true,
     dotsClass: 'slick-dots',
     centerMode: true,
-    centerPadding: '10px',
+    centerPadding: '100px',
     slidesToShow: 1,
     slidesToScroll: 4,
     responsive: [
@@ -34,5 +34,30 @@ $(document).ready(function(){
       }
     ]
   });
+
+  function setSlideVisibility() {
+  //Find the visible slides i.e. where aria-hidden="false"
+    var visibleSlides = $('.slick-slide[aria-hidden="false"]');
+    //Make sure all of the visible slides have an opacity of 1
+    $(visibleSlides).each(function() {
+      $(this).css('opacity', 1);
+    });
+    //Set the opacity of the first and last partial slides.
+    $(visibleSlides).first().prev().css('opacity', 0);
+    $(visibleSlides).last().next().css('opacity', 0);
+  }
+
+  $(setSlideVisibility());
+
+  $('.workCarousel').on('beforeChange', function() {
+    $('.slick-slide').each(function() {
+      $(this).css('opacity', 1);
+    });
+  });
+
+  $('.workCarousel').on('afterChange', function() {
+    setSlideVisibility();
+  });
+
 
 });
